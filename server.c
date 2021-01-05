850ca240c423ea100c9b9bcc7e3cb2cc499620ed
zDonik <tokhirovdoniyor@gmail.com>
2021-01-05 21:57:35 +0400

    feat: added renting rooms

--- server.c
+++ server.c
@@ -126,7 +126,7 @@ void login(sqlite3 *db, int *client_id, json_object **result)
 void build_list(sqlite3 *db, json_object **result)
 {
     *result = json_object_new_array();
-    if (execute_query(db, "select * from houses", *result)) {
+    if (execute_query(db, "select * from houses where isBusy=0", *result)) {
         json_object_put(*result);
         *result = json_object_new_object();
         json_object_object_add(*result, "error", json_object_new_string("server error: sql query failed"));
@@ -223,7 +223,24 @@ void remove_room(int client_id, sqlite3 *db, json_object **result)
 
 void rent_room(int client_id, sqlite3 *db, json_object **result)
 {
-    // rent a room
+    if (!is_logged_in(client_id, result)) {
+        return;
+    }
+
+    char *house_id = strtok(NULL, " ");
+
+    char query[200];
+    sprintf(query, "update houses set isBusy=1 where house_id=%s", house_id);
+    *result = json_object_new_array();
+    if (execute_query(db, query, *result)) {
+        json_object_put(*result);
+        *result = json_object_new_object();
+        json_object_object_add(*result, "error", json_object_new_string("could not rent house"));
+    } else {
+        json_object_put(*result);
+        *result = json_object_new_object();
+        json_object_object_add(*result, "info", json_object_new_string("successfully rented the house"));
+    }
 }
 
 // ---- end ----
