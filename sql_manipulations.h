3be3adcaf6827e4d6c93798da5452356b93addce
zDonik <tokhirovdoniyor@gmail.com>
2021-01-05 22:19:43 +0400

    fix: removed unnecessary functions

--- sql_manipulations.h
+++ sql_manipulations.h
@@ -10,8 +10,4 @@ int execute_query(sqlite3 *db, const char *query, json_object *json_array);
 // callback for query result
 int process_result(void *param, int size, char **data, char **columns);
 
-int db_remove(sqlite3 *db, json_object *json_array);
-int db_update(sqlite3 *db, json_object *json_array);
-int db_insert(sqlite3 *db, json_object *json_array);
-
 #endif // SQL_MANIPULATIONS_H
